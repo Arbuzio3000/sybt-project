3ec825942c790597e27833c18f73744a69450154
Karen <karen.oganesyan2004@gmail.com>
2024-05-15 20:39:22 +0400

    add active click on header links

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,13 @@
+$('.header__link').click( function() {
+    $('.header__link')
+        .removeClass('active')
+    $(this)
+        .addClass('active')
+});
+
+
 $('.promo__btn').hover( function() {
     $(this)
         .addClass('promo__btn_active').siblings().removeClass('promo__btn_active')
 });
-
 
